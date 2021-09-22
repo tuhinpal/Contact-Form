43e7f0197666c649cfbb1607b78cb2ee1182753d
Anshuman Fauzdar <fauzdaranshuman@gmail.com>
2021-09-22 13:31:54 +0530

    test with github

--- src/contact-form.js
+++ src/contact-form.js
@@ -10,7 +10,7 @@ window.onload = () => {
   var cfstylesheet = document.createElement("link");
   cfstylesheet.rel = "stylesheet";
   cfstylesheet.href =
-    "https://cdn.jsdelivr.net/gh/anshumanfauzdar/contact-form@v2/src/style.css";
+    "https://raw.githubusercontent.com/AnshumanFauzdar/Contact-Form/main/src/style.css";
   document.getElementsByTagName("head")[0].appendChild(cfstylesheet);
 
   cfstylesheet.onload = function () {
