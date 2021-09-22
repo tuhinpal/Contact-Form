b98ea321294261edac2e33836a7660563de198e1
Anshuman Fauzdar <fauzdaranshuman@gmail.com>
2021-09-22 13:56:02 +0530

    v3.0.0

--- src/contact-form.js
+++ src/contact-form.js
@@ -10,7 +10,7 @@ window.onload = () => {
   var cfstylesheet = document.createElement("link");
   cfstylesheet.rel = "stylesheet";
   cfstylesheet.href =
-    "https://cdn.jsdelivr.net/gh/anshumanfauzdar/contact-form@main/src/style.css";
+    "https://cdn.jsdelivr.net/gh/anshumanfauzdar/contact-form@v3/src/style.css";
   document.getElementsByTagName("head")[0].appendChild(cfstylesheet);
 
   cfstylesheet.onload = function () {
